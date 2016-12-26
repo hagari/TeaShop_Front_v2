5fb3c39db8a1d106df62b1999ea2c3a60d199557
Hagari <hagari07@gmail.com>
2016-12-26 04:06:00 +0200

    got controllers' def out of the route

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 (function() {
+
   angular.module('teaStore', [
       'store-directives',
       'ngRoute',
@@ -10,48 +11,17 @@
       $routeProvider
           .when('/', {
               templateUrl: 'templates/home.html',
-              controller: function($http){
-                  var ctrl = this;
-                  ctrl.getHomePage  = function(){
-                      $http.get('/')
-                          .then(function(response) {
-                              ctrl.products = response.data;
-                          });
-                  }
-              },
+              controller: 'HomeController',
               controllerAs: 'homeCtrl'
           })
           .when('/products', {
                   templateUrl: 'templates/products.html',
-                  controller: function($http){
-                      var ctrl = this;
-
-                      ctrl.getProducts = function() {
-                          $http.get('/products')
-                              .then(function(response) {
-                              ctrl.products = response.data;
-                              console.log("inside $http.get('/products')");
-                              console.log(response.data.toString());
-                          });
-                      };
-
-                      ctrl.getProducts();
-                  },
+                  controller: 'ProductsController',
                   controllerAs: 'productsCtrl'
               })
           .when('/products/:id', {
               templateUrl: 'productShow.html',
-              controller: function($http, $routeParams){
-                  var ctrl = this;
-
-                  ctrl.getProductById = function () {
-                      $http.get('/products/' + $routeParams.id)
-                          .then(function(response){
-                              ctrl.products = response.data;
-                          });
-                  }
-                  ctrl.getProductById();
-              },
+              controller: 'ProductShowController',
               controllerAs: 'showCtrl'})
           .when('/cart', {
               templateUrl: 'templates/cart.html',
@@ -61,30 +31,104 @@
           .otherwise({redirectTo: '/'});
   }]);
 
+    angular.module('teaStore').service('teasService', function() {
+        var teas = [
+            {
+                id:1,
+                name:'White Tea',
+                price:17.5,
+                type:'White',
+                details:'Delicate, premium white tea subtly flavored with natural, sweet Arabian jasmin flowers.',
+                soldOut:false,
+                canPurchase:true,
+                image:"02_white-tea-with-jasmine"},
+            {
+                id:2,
+                name:'Green Tea',
+                price:19.5,
+                type:'Green',
+                details:'Delicate, premium white tea subtly flavored with natural, sweet Arabian jasmin flowers.',
+                soldOut:false,
+                canPurchase:true,
+                image:"03_green-tea"},
+        ]
+
+        this.getTeas = function(){
+            return teas;
+        }
+
+        this.addTea = function(){
+            //..
+        }
+
+    });
+
     angular.module('teaStore').controller('StoreController', function() {
         this.products = teas;
   });
 
-  var teas = [
-      {
-        id:1,
-        name:'White Tea',
-        price:17.5,
-        type:'White',
-        details:'Delicate, premium white tea subtly flavored with natural, sweet Arabian jasmin flowers.',
-        soldOut:false,
-        canPurchase:true,
-        image:"02_white-tea-with-jasmine"},
-      {
-          id:2,
-          name:'Green Tea',
-          price:19.5,
-          type:'Green',
-          details:'Delicate, premium white tea subtly flavored with natural, sweet Arabian jasmin flowers.',
-          soldOut:false,
-          canPurchase:true,
-          image:"03_green-tea"},
-]
+    angular.module('teaStore').controller('HomeController', ['teasService', '$http', function (teasService,$http) {
+        var ctrl = this;
 
+        ctrl.products = teasService.getTeas();
+        ctrl.getHomeProducts = function() {
+            $http.get('/')
+                .then(function(response) {
+                    ctrl.products = response.data;
+                });
+        }
+
+        ctrl.getHomeProducts();
+    }]);
+
+    angular.module('teaStore').controller('ProductsController', ['teasService', '$http', function (teasService,$http) {
+        var ctrl = this;
+
+        ctrl.products = teasService.getTeas();
+        ctrl.getProducts = function() {
+            $http.get('/products')
+                .then(function(response) {
+                    ctrl.products = response.data;
+                });
+        };
+
+        ctrl.getProducts();
+    }]);
+
+    angular.module('teaStore').controller('ProductShowController', ['teasService', '$http', function (teasService,$http) {
+        var ctrl = this;
+
+        ctrl.products = teasService.getTeas();
+
+        ctrl.getProductById = function () {
+            $http.get('/products/' + $routeParams.id)
+                .then(function(response){
+                    ctrl.products = response.data;
+                });
+        }
+        ctrl.getProductById();
+    }]);
+
+
+  var teas = [
+        {
+            id:1,
+            name:'White Tea',
+            price:17.5,
+            type:'White',
+            details:'Delicate, premium white tea subtly flavored with natural, sweet Arabian jasmin flowers.',
+            soldOut:false,
+            canPurchase:true,
+            image:"02_white-tea-with-jasmine"},
+        {
+            id:2,
+            name:'Green Tea',
+            price:19.5,
+            type:'Green',
+            details:'Delicate, premium white tea subtly flavored with natural, sweet Arabian jasmin flowers.',
+            soldOut:false,
+            canPurchase:true,
+            image:"03_green-tea"},
+    ]
   var cart = [];
 })();
